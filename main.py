7ee0c12f3287ce9421f2693d9ae1bc8a991d2db6
Graham Heathcote <grahamkheathcote@gmail.com>
2026-08-11 03:23:18 -0500

    fix

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ def generate_training_data(samples):
     env = gym.make("gym_pusht/PushT-v0")
     arr = np.empty((12, samples))
     env.reset()
+    observation, _, _, _, _ = env.step(env.action_space.sample())
     for i in range(samples):
-        observation, reward, terminated, truncated, _ = env.step(env.action_space.sample())
         arr[:5, i] = observation
         action = env.action_space.sample()
         arr[5:7, i] = action
         observation, reward, terminated, truncated, _ = env.step(action)
         arr[7:12, i] = observation
     return arr
-
