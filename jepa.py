3b2fc0ef753ad6aee6946c6f73131d01297ac3a9
Graham Heathcote <grahamkheathcote@gmail.com>
2026-08-12 00:27:18 -0500

    jepa

---
+++ jepa.py
@@ -0,0 +1,8 @@
+from neural_network import Network
+
+class Jepa():
+    def __init__(self, latent_dims, enc1, enc2, pred1):
+        self.encoder = Network(12, enc1, enc2, latent_dims)
+        self.predictor = Network(latent_dims, pred1, latent_dims)
+        
+        
